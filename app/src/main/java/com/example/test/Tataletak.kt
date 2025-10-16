ab5062010688ff08b711bfca4af20aeeb65a77a5
hanifatulnadiva <161436287+hanifatulnadiva@users.noreply.github.com>
2025-10-16 15:49:04 +0700

    menambahkan layout column di fungsi tataletakboxColumnrow

--- app/src/main/java/com/example/test/Tataletak.kt
+++ app/src/main/java/com/example/test/Tataletak.kt
@@ -1,5 +1,6 @@
 package com.example.test
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -7,6 +8,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -115,7 +117,9 @@ fun TataletakRowCulumn(modifier: Modifier)
 }
 
 @Composable
-fun TataletakBoxColumnRow(modifier: Modifier)
-{
+fun TataletakBoxColumnRow(modifier: Modifier){
+    Column {
 
+    }
 }
+
